c3da14102ccd5414ea3241e48544bf620ae8dc56
jonasbergstrom <jontrian97@gmail.com>
2021-04-28 16:54:39 +0200

    Made some changes in the css to make the image fit with the same size for all the dogs

--- src/App.js
+++ src/App.js
@@ -6,24 +6,19 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      names: [],
-      pageNum: 1,
+      names: []
+    
       
     }
 
   }
   componentDidMount() {
-    const url = "https://api.jsonbin.io/b/607eb43024143e5df089b745";
+    const url = "https://api.jsonbin.io/b/60882ce55210f622be3b4768";
    
-
-    
     fetch(url) 
       .then(data => data.json())
-      .then(data => this.setState({ names: data}))
-
-
+      .then(data => this.setState({ names: data.dogs}))
     }
-
   
   render() {
     const { names } = this.state;
@@ -36,7 +31,7 @@ class App extends Component {
         <div className="users">
           {names.map((user) => (
             <div className="card" key={user.chipNumber}>
-            <img src={user.img} class="card-img-top" alt={`${user.name} img`} />
+            <img src={user.img} className="card-img-top" alt={`${user.name} img`} />
             <div className="card-body">
               <h5 className="card-title">{user.name} {user.sex}</h5>
               <p className="card-text">Owners name is:{user.owner.name}</p>
@@ -45,16 +40,8 @@ class App extends Component {
             </div>
 
           ))}
+              </div>
           </div>
-
-          <div className="btn-group" role="group" aria-label="Basic example">
-          <button type="button" class="btn btn-primary">Prev</button>
-          <button type="button" class="btn btn-primary">{this.state.pageNum}</button>
-          <button type="button" class="btn btn-primary">Next</button>
-            </div>   
-          </div>
-
-    
     )
   }
  
